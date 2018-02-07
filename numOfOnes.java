33df9ff1a7bdf38a313cbd18b9c947b6a2e1cadf
Aditi Lonhari <lonhari.aditi@gmail.com>
2018-02-07 13:47:19 -0800

    Create numOfOnes.java

---
+++ numOfOnes.java
@@ -0,0 +1,13 @@
+public class Solution {
+
+	public int numSetBits(long a) {
+	    int count = 0;
+        
+        while (a > 0)
+        {
+            a &= (a - 1) ;
+            count++;
+        }
+        return count;
+	}
+}
